5c642b803f29e93f673379ddee3f582643a3a487
Asad Siddiqui <msasad8@gmail.com>
2019-03-19 10:44:48 +0530

    Add status bar

--- src/App.js
+++ src/App.js
@@ -41,8 +41,12 @@ class StatusBar extends Component {
       <div className="status">
         <div className={`status-white ${this.props.black_turn ? '' : 'active'}`}>
           {this.props.white}
+          <div className="circle white"></div>
+          <div className="arrow">←</div>
         </div>
         <div className={`status-black ${this.props.black_turn ? 'active' : ''}`}>
+          <div className="arrow">→</div>
+          <div className="circle black"></div>
           {this.props.black}
         </div>
       </div>
