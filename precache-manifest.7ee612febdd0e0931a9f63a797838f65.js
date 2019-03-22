77f7cccc9c32cc1743280f8f245a560176f81123
Asad Siddiqui <msasad8@gmail.com>
2019-03-22 20:40:53 +0530

    Updates

---
+++ precache-manifest.7ee612febdd0e0931a9f63a797838f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-reversi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68517de9f1e04e13ffef",
+    "url": "/react-reversi/static/js/runtime~main.68517de9.js"
+  },
+  {
+    "revision": "965b389b3c21bb690995",
+    "url": "/react-reversi/static/js/main.965b389b.chunk.js"
+  },
+  {
+    "revision": "f370b753e5901661dd5d",
+    "url": "/react-reversi/static/js/1.f370b753.chunk.js"
+  },
+  {
+    "revision": "965b389b3c21bb690995",
+    "url": "/react-reversi/static/css/main.656e7290.chunk.css"
+  },
+  {
+    "revision": "19058c40456ac7eb1919041408f4db68",
+    "url": "/react-reversi/index.html"
+  }
+];
